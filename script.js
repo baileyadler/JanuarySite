e250de6d9e419df30c050b3e45296a15ba92b976
baileyadler <526BAdler@frhsd.com>
2025-01-14 09:25:24 -0500

    changed font text and added padding

---
+++ script.js
@@ -0,0 +1,47 @@
+const getComputerChoice = () => {
+  const randomNumber = Math.floor(Math.random() * 3);
+  switch (randomNumber) {
+    case 0:
+      return "rock";
+    case 1:
+      return "paper";
+    case 2:
+      return "scissors";
+  }
+};
+const determineWinner = (userChoice, computerChoice) => {
+  if (userChoice === computerChoice) {
+    return "The game is a tie.";
+  }
+  if (userChoice === "rock") {
+    if (computerChoice === "paper") {
+      return "The computer has won.";
+    } else {
+      return "The user has won.";
+    }
+  }
+  if (userChoice === "paper") {
+    if (computerChoice === "scissors") {
+      return "The computer won.";
+    } else {
+      return "The user won.";
+    }
+  }
+  if (userChoice === "scissors") {
+    if (computerChoice === "rock") {
+      return "The computer won.";
+    } else {
+      return "The user won.";
+    }
+  }
+};
+if (userChoice === "bomb") {
+  return "You won!";
+}
+
+const playGame = (userChoice) => {
+  const computerChoice = getComputerChoice();
+  console.log(`You chose: ${userChoice}.`);
+  console.log(`Computer chose: ${computerChoice}.`);
+  console.log(determineWinner(userChoice, computerChoice));
+};
