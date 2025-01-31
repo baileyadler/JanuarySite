5feff2034b915ac1f5105d0da79a18d8960c2c5d
baileyadler <526BAdler@frhsd.com>
2025-01-31 02:10:38 +0000

    javascript now works

--- script.js
+++ script.js
@@ -1,79 +1,56 @@
+
 let userGreet = alert("Please follow the directions");
 let userName = prompt("Write your name");
 userName
   ? (document.getElementById("greeting").innerText = `Hello, ${userName}!`)
   : (document.getElementById("greeting").innerText = `Hello!`);
 
-let userScore = 0;
+const choices = ["rock", "paper","scissors"];
+const playerDisplay = document.getElementById("playerDisplay");
+const computerDisplay = document.getElementById("computerDisplay");
+const resultDisplay = document.getElementById("resultDisplay");
+const playerScoreDisplay = document.getElementById("playerScoreDisplay");
+const computerScoreDisplay = document.getElementById("computerScoreDisplay");
+
+let playerScore = 0;
 let computerScore = 0;
 let roundNumber = 0;
-let resultChoice;
-function getComputerChoice() {
-  const randomNumber = Math.floor(Math.random() * 3);
+function playGame(playerChoice){
+  roundNumber++;
 
-  switch (randomNumber) {
-    case 0:
-      return "rock";
-    case 1:
-      return "paper";
-    case 2:
-      return "scissors";
+  if (roundNumber > 5) {
+    alert('Game Over. Thanks for playing');
+      }
+
+  const computerChoice = choices[Math.floor(Math.random() * 3)];
+let result = "";
+if(playerChoice === computerChoice){
+  result = "Tie Game";
+} else{
+  switch(playerChoice){
+    case "rock":
+   result = (computerChoice === "scissors") ? "You Win" : "Computer Win";
+   break;
+   case "paper":
+   result = (computerChoice === "rock") ? "You Win" : "Computer Win";
+   break;
+   case "scissors":
+   result = (computerChoice === "paper") ? "You Win" : "Computer Win";
+   break;
   }
 }
-const getResult = (userChoice, computerChoice) => {
-  if (userChoice === computerChoice) {
-    return "The game is a tie.";
-  }
-  if (userChoice === "rock") {
-    if (computerChoice === "paper") {
-      return "The computer has won.";
-    } else {
-      return "The user has won.";
-    }
-  }
-  if (userChoice === "paper") {
-    if (computerChoice === "scissors") {
-      return "The computer won.";
-    } else {
-      return "The user won.";
-    }
-  }
-  if (userChoice === "scissors") {
-    if (computerChoice === "rock") {
-      return "The computer won.";
-    } else {
-      return "The user won.";
-    }
-  }
-};
-
-const computerChoice = getComputerChoice();
-const result = getResult(userChoice, computerChoice);
-
-
-
-document.getElementById("userResult").textContent = `User score: ${userResult}`;
-document.getElementById("computerResult").textContent = `Computer Score ${computerResult}`;
-document.getElementById("resultChoice").textContent = result;
-document.getElementById("userResult").textContent = `user: ${userResult}.`;
-document.getElementById("computerResult").textContent = `computer: ${computerResult}.`;
-document.getElementById("resultChoice").textContent = `result: ${resultChoice}.`;
-
-if (result === "The user has won") {
-  userScore++;
-} else if (result === "The computer has won") {
-  computerScore++;
+switch(result){
+  case "You Win":
+    playerScore++;
+    playerScoreDisplay.textContent = playerScore;
+    break;
+    case "Computer Win":
+    computerScore++;
+    computerScoreDisplay.textContent = computerScore;
+    break;
 }
-roundNumber++;
-if (roundNumber === 5) {
-  if (userScore > computerScore) {
-    console.log("User wins");
-  } else if (userScore < computerScore) {
-    console.log("Computer won");
-  } else {
-    console.log("Tie game");
-  }
-  console.log("Game Over");
-  alert("Game is over. Thanks for playing.");
+playerDisplay.textContent = `PLAYER: ${playerChoice}`;
+computerDisplay.textContent = `COMPUTER: ${computerChoice}`;
+resultDisplay.textContent = result;
 }
 
